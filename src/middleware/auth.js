596e901c6252d97228112ab88a1a7c2718958892
PheonixOfTesla <digitalappleco@gmail.com>
2025-09-16 14:46:48 -0400

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,6 +4,11 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
     let token;
     
+// Allow OPTIONS requests to pass through
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
+    
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
