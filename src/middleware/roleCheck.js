80de5a9f9192782a05a03fd0371861b0cc8ed850
PheonixOfTesla <digitalappleco@gmail.com>
2025-09-16 23:43:46 -0400

    Update roleCheck.js

--- src/middleware/roleCheck.js
+++ src/middleware/roleCheck.js
@@ -1,6 +1,11 @@
-const checkRole = (roles) => {
-    // Accept either a single array or multiple arguments
-    const requiredRoles = Array.isArray(roles) ? roles : Array.from(arguments);
+const checkRole = (...roles) => {
+    // Flatten the roles array in case an array was passed as first argument
+    let requiredRoles = roles;
+    
+    // If the first argument is an array, use it as the roles list
+    if (roles.length === 1 && Array.isArray(roles[0])) {
+        requiredRoles = roles[0];
+    }
     
     return (req, res, next) => {
         console.log('CheckRole middleware - Required roles:', requiredRoles);
@@ -15,7 +20,7 @@ const checkRole = (roles) => {
             });
         }
         
-        // Ensure roles is an array
+        // Ensure user roles is an array
         const userRoles = Array.isArray(req.user.roles) ? req.user.roles : [req.user.roles].filter(Boolean);
         
         console.log('CheckRole middleware - Checking roles:', userRoles, 'against required:', requiredRoles);
